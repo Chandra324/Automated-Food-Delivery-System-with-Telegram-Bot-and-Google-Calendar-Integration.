ebd1b97dba13cd618c80a5a365ecad4349a276f8
Chandra Sekhar Reddy Kummetha <114323886+Chandra324@users.noreply.github.com>
2025-01-08 16:56:14 +0530

    Delete FoodDeliveryTelegram-GC directory
    delete

--- FoodDeliveryTelegram-GC/src/main/java/FoodApp/FoodDeliveryTelegram/GC/Repository/FoodItemRepository.java
+++
@@ -1,8 +0,0 @@
-package FoodApp.FoodDeliveryTelegram.GC.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.FoodApp.FoodDeliveryTelegram.GC.Entity.FoodItem;
-
-public interface FoodItemRepository extends JpaRepository<FoodItem, Integer> {}
-
